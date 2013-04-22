0142f036d049ba72cc3ec40d96227ded426305e1
Josh Taylor <joshotaylor@gmail.com>
2013-04-22 22:27:43 +0100

    Implemented base template for showing all and individual note models

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('notes', { path: '/' });
+	this.resource('notes', { path: '/' }, function() {
+		this.resource('note', { path: '/notes/:note_id'});
+
+		this.route('new');
+		this.route('my_notes');
+		this.route('search');
+	});
 })
 
 App.Store = DS.Store.extend({
-	revision: 12
+	revision: 12,
+	adapter: 'DS.FixtureAdapter'
 })
 
 App.Note = DS.Model.extend({
 	name: DS.attr('string'),
-	content: DS.attr('string')
+	note: DS.attr('string')
 })
 
+App.NotesIndexRoute = Ember.Route.extend({
+	model: function() {
+		return App.Note.find();
+	}
+})
--- js/fixtures.js
+++ js/fixtures.js
@@ -2,11 +2,11 @@ App.Note.FIXTURES = [
 	{
 		id: 1,
 		name: "Sub-menu list item width",
-		content: "<p>How to solve the issue</p><pre>li {<br />&nbsp;&nbsp;&nbsp;&nbsp;white-space: nowrap;<br />}</pre>"
+		note: "<p>How to solve the issue:</p><pre>li {<br />&nbsp;&nbsp;&nbsp;&nbsp;white-space: nowrap;<br />}</pre>"
 	},
 	{
 		id: 2,
 		name: "Another scratchpad note",
-		content: "<p>Yet another scratchpad note</p>"
+		note: "<p>Yet another scratchpad note</p>"
 	}
 ]
