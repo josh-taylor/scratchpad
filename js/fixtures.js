aff64efee999603dd1f1326403a56e3abe91a9d2
Josh Taylor <josh@somarketing.com>
2013-04-23 13:36:19 +0100

    Added more fixtures

--- js/fixtures.js
+++ js/fixtures.js
@@ -10,6 +10,18 @@ App.Note.FIXTURES = [
 		name: "Another scratchpad note",
 		note: "<p>Yet another scratchpad note</p>",
 		tags: [1]
+	},
+	{
+		id: 3,
+		name: "A JavaScript issue",
+		note: "<p>Stopping jQuery conflicting</p><pre>jQuery.noConflict();</pre>",
+		tags: [3]
+	},
+	{
+		id: 4,
+		name: "An issue containing everything",
+		note: "<p>This is such a generic note it needs all tags</p>",
+		tags: [1,2,3]
 	}
 ]
 
@@ -17,16 +29,16 @@ App.Tag.FIXTURES = [
 	{
 		id: 1,
 		name: "HTML",
-		posts: [1, 2]
+		posts: [1,2,4]
 	},
 	{
 		id: 2,
 		name: "CSS",
-		posts: [1]
+		posts: [1,4]
 	},
 	{
 		id: 3,
 		name: "JavaScript",
-		posts: []
+		posts: [3,4]
 	}
 ]
